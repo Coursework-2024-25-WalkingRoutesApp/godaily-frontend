57bcb30879b54756fa566abb6f4bb11275b242d0
yuulkht <juliakukhtina12@gmail.com>
2025-01-09 21:13:43 +0300

    feat: add switch buttons

---
+++ app/src/main/java/ru/hse/coursework/godaily/ui/components/quarks/FavouriteToggle.kt
@@ -0,0 +1,45 @@
+package ru.hse.coursework.godaily.ui.components.quarks
+
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import ru.hse.coursework.godaily.R
+import ru.hse.coursework.godaily.ui.theme.greyDark
+import ru.hse.coursework.godaily.ui.theme.lime
+
+@Composable
+fun FavouriteToggle(
+    isFavorite: Boolean,
+    onToggle: (Boolean) -> Unit
+) {
+    IconButton(
+        onClick = { onToggle(!isFavorite) },
+        modifier = Modifier.size(40.dp, 35.dp)
+    ) {
+        Icon(
+            painter = if (isFavorite) painterResource(id = R.drawable.heart_enabled) else painterResource(
+                id = R.drawable.heart_disabled
+            ),
+            contentDescription = null,
+            modifier = Modifier.size(35.dp, 32.dp),
+            tint = if (isFavorite) lime else greyDark
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun FavouriteTogglePreview() {
+    val isFavorite = remember { mutableStateOf(false) }
+    FavouriteToggle(
+        isFavorite = isFavorite.value,
+        onToggle = { isFavorite.value = it }
+    )
+}
