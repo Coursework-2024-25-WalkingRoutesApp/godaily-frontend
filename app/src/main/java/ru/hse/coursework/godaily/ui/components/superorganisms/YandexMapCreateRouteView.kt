620572ae04408a3d8445a8bf6158649d7ae15aab
yuulkht <juliakukhtina12@gmail.com>
2025-03-19 21:56:25 +0300

    feat: add geosuggest to map while creating route

--- app/src/main/java/ru/hse/coursework/godaily/ui/components/superorganisms/YandexMapCreateRouteView.kt
+++ app/src/main/java/ru/hse/coursework/godaily/ui/components/superorganisms/YandexMapCreateRouteView.kt
@@ -2,9 +2,14 @@ package ru.hse.coursework.godaily.ui.components.superorganisms
 
 import android.util.Log
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
@@ -12,6 +17,7 @@ import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.toArgb
@@ -22,17 +28,26 @@ import com.yandex.mapkit.Animation
 import com.yandex.mapkit.MapKitFactory
 import com.yandex.mapkit.RequestPoint
 import com.yandex.mapkit.RequestPointType
+import com.yandex.mapkit.geometry.BoundingBox
 import com.yandex.mapkit.geometry.Point
 import com.yandex.mapkit.map.CameraPosition
 import com.yandex.mapkit.map.IconStyle
 import com.yandex.mapkit.map.InputListener
 import com.yandex.mapkit.map.Map
+import com.yandex.mapkit.map.VisibleRegionUtils
 import com.yandex.mapkit.mapview.MapView
+import com.yandex.mapkit.search.Response
+import com.yandex.mapkit.search.SearchFactory
+import com.yandex.mapkit.search.SearchManagerType
+import com.yandex.mapkit.search.SearchOptions
+import com.yandex.mapkit.search.SuggestItem
+import com.yandex.mapkit.search.SuggestOptions
+import com.yandex.mapkit.search.SuggestResponse
+import com.yandex.mapkit.search.SuggestSession
 import com.yandex.mapkit.transport.TransportFactory
 import com.yandex.mapkit.transport.masstransit.FitnessOptions
 import com.yandex.mapkit.transport.masstransit.Route
 import com.yandex.mapkit.transport.masstransit.RouteOptions
-import com.yandex.mapkit.transport.masstransit.Session
 import com.yandex.mapkit.transport.masstransit.TimeOptions
 import com.yandex.runtime.Error
 import com.yandex.runtime.image.ImageProvider
@@ -42,9 +57,12 @@ import ru.hse.coursework.godaily.R
 import ru.hse.coursework.godaily.core.domain.routesession.TitledPoint
 import ru.hse.coursework.godaily.ui.components.molecules.Cancel
 import ru.hse.coursework.godaily.ui.components.organisms.AddPointTitleDialog
+import ru.hse.coursework.godaily.ui.components.organisms.SearchBarWithSuggestions
 import ru.hse.coursework.godaily.ui.notification.ToastManager
 import ru.hse.coursework.godaily.ui.theme.purpleRoutes
 import java.util.UUID
+import com.yandex.mapkit.search.Session as SearchSession
+import com.yandex.mapkit.transport.masstransit.Session as RouteSession
 
 // TODO геосаджест
 // TODO тап на точку
@@ -69,9 +87,24 @@ fun YandexMapCreateRouteView(
     val selectedPoint = remember { mutableStateOf<TitledPoint?>(null) }
     val isAddingPoint = remember { mutableStateOf(false) }
 
+    val searchQuery = remember { mutableStateOf("") }
+    val suggestItems = remember { SnapshotStateList<SuggestItem>() }
 
     val mapView = remember { MapView(context) }
     val coroutineScope = rememberCoroutineScope()
+
+    val searchManager = SearchFactory.getInstance().createSearchManager(SearchManagerType.COMBINED)
+    val suggestSession = searchManager.createSuggestSession()
+    val suggestOptions = SuggestOptions()
+
+    val suggestListener = object : SuggestSession.SuggestListener {
+        override fun onResponse(p0: SuggestResponse) {
+            suggestItems.clear()
+            suggestItems.addAll(p0.items.take(5))
+        }
+
+        override fun onError(error: Error) {}
+    }
 
     val inputListener = remember(context) {
         object : InputListener {
@@ -137,18 +170,104 @@ fun YandexMapCreateRouteView(
             }
         )
 
-        if (routePoints.isNotEmpty()) {
-            Cancel(
-                onClick = {
-                    routePoints.removeLastOrNull()
-                    mapView.mapWindow.map.mapObjects.clear()
-                    updateRoute(mapView.mapWindow.map, routePoints, startIcon, midIcon, endIcon)
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+        ) {
+            SearchBarWithSuggestions(
+                query = searchQuery,
+                suggestions = suggestItems,
+                onValueChange = {
+                    suggestSession.suggest(
+                        searchQuery.value,
+                        BoundingBox(
+                            mapView.mapWindow.map.visibleRegion.bottomLeft,
+                            mapView.mapWindow.map.visibleRegion.topRight
+                        ),
+                        suggestOptions,
+                        suggestListener,
+                    )
                 },
+                onSearchClick = {
+                    searchManager.submit(
+                        searchQuery.value,
+                        VisibleRegionUtils.toPolygon(mapView.mapWindow.map.visibleRegion),
+                        SearchOptions(),
+                        object : SearchSession.SearchListener {
+                            override fun onSearchResponse(response: Response) {
+                                val searchResults = response.collection.children
+                                if (searchResults.isNotEmpty()) {
+                                    val point = searchResults[0].obj?.geometry?.get(0)?.point
+                                    if (point != null) {
+                                        mapView.mapWindow.map.mapObjects.addPlacemark().apply {
+                                            geometry = point
+                                            setIcon(
+                                                ImageProvider.fromResource(
+                                                    context,
+                                                    R.drawable.search_placemark
+                                                )
+                                            )
+                                            setIconStyle(IconStyle().apply { scale = 0.5f })
+                                        }
+                                        mapView.mapWindow.map.move(
+                                            CameraPosition(point, 16.0f, 0.0f, 0.0f),
+                                            Animation(Animation.Type.SMOOTH, 1f),
+                                            null
+                                        )
+                                    }
+                                }
+                            }
+
+                            override fun onSearchError(error: Error) {
+                                Log.e("SearchError", "Произошла ошибка при поиске: $error")
+                            }
+                        }
+                    )
+                },
+                onSuggestionClick = { suggestion ->
+                    suggestion.center?.let { point ->
+                        mapView.mapWindow.map.mapObjects.addPlacemark().apply {
+                            geometry = point
+                            setIcon(
+                                ImageProvider.fromResource(
+                                    context,
+                                    R.drawable.search_placemark
+                                )
+                            )
+                            setIconStyle(IconStyle().apply { scale = 0.5f })
+                        }
+
+                        mapView.mapWindow.map.move(
+                            CameraPosition(
+                                point,
+                                14f,
+                                0f,
+                                0f
+                            ),
+                            Animation(Animation.Type.SMOOTH, 1f),
+                            null
+                        )
+                    }
+                }, //todo
                 modifier = Modifier
-                    .align(Alignment.TopEnd)
-                    .padding(16.dp)
-                    .size(40.dp)
+                    .align(Alignment.Top)
+                    .width(330.dp)
             )
+            Spacer(modifier = Modifier.weight(1f))
+
+            if (routePoints.isNotEmpty()) {
+                Cancel(
+                    onClick = {
+                        routePoints.removeLastOrNull()
+                        mapView.mapWindow.map.mapObjects.clear()
+                        updateRoute(mapView.mapWindow.map, routePoints, startIcon, midIcon, endIcon)
+                    },
+                    modifier = Modifier
+                        .align(Alignment.Top)
+                        .padding(top = 16.dp, end = 16.dp)
+                        .size(40.dp)
+                        .widthIn(min = 40.dp)
+                )
+            }
         }
     }
 
@@ -196,7 +315,7 @@ private fun updateRoute(
         requestPoints,
         TimeOptions(),
         RouteOptions(FitnessOptions(false, false)),
-        object : Session.RouteListener {
+        object : RouteSession.RouteListener {
             override fun onMasstransitRoutes(pedestrianRoutes: MutableList<Route>) {
                 pedestrianRoutes.firstOrNull()?.let { route ->
                     map.mapObjects.clear()
@@ -237,5 +356,4 @@ private fun setPlacemarks(
             setIconStyle(IconStyle().apply { scale = 0.7f })
         }
     }
-
 }
